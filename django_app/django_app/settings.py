214f3d0411167b85d721d77d91495e8a6d211e67
ben <2390188@mail.ru>
2025-12-10 08:50:51 +0300

    поправил cors чтобы посылать запросы из браузера

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -34,6 +34,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
+
+
 
 # Application definition
 
@@ -49,9 +55,17 @@ INSTALLED_APPS = [
 
     'rest_framework',  # Django REST Framework
     'feedback',  # Приложение для отзывов
+
+
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
